78336feffb30c72e914066cae4c5d28d7146a156
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2014-09-16 15:27:58 +0700

    Initial Commit

---
+++ Histogram/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Histogram
+//
+//  Created by Nuttapon Pattanavijit on 9/16/14.
+//  Copyright (c) 2014 nut. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
